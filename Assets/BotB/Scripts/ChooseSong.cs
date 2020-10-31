66ac68ae6d7f2b83097fcb81182d28c441cacc09
connord627 <connor.davidson@tufts.edu>
2020-10-31 15:41:23 -0400

    Song Selection Menu
    All 3 buttons call the same song right now

---
+++ Assets/BotB/Scripts/ChooseSong.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChooseSong : MonoBehaviour
+{
+    public int SongNumber;
+    public void changeToScene(int sceneToChangeTo)
+    {
+        SceneManager.LoadScene(sceneToChangeTo);
+    }
+}
