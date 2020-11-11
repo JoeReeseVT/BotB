14b18e314740b5bc8301e348c9c9ec5215f685cb
connord627 <connor.davidson@tufts.edu>
2020-11-11 15:23:55 -0500

    button mapping assets

---
+++ Assets/BotB/Scripts/MainMenuScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuScript : MonoBehaviour
+{
+    //menu states
+    public enum MenuStates { Main, Controls};
+    public MenuStates currentState;
+
+    void Awake()
+    {
+        currentState = MenuStates.Main;
+    }
+
+    void Update()
+    {
+        switch (currentState)
+        {
+            case MenuStates.Main:
+                break;
+
+            case MenuStates.Controls:
+                break;
+
+
+        }
+    }
+
+    public void OnControls()
+    {
+        
+    }
+
+    public void OnMainMenu()
+    {
+        
+    }
+}
