8534a4b1214c469ed4b793e559164e5feef11e47
Joe Reese <joereese.vt@gmail.com>
2020-12-08 18:08:47 -0500

    Add basic sound triggering to anim FSM
    Includes adding AudioSource components to the player prefabs

---
+++ Assets/BotB/Scripts/StateBehaviors/SoundFXTrigger.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundFXTrigger : StateMachineBehaviour
+{
+    // Name of the audio clip we will direct our audioSource to play
+    public AudioClip audioClip;
+    public float delayTime;
+
+    private float timer;
+    private bool played;
+
+    // OnStateEnter is called before OnStateEnter is called on any state inside this state machine
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        timer = Time.time;
+        played = false;
+        //animator.transform.root.gameObject.GetComponent<AudioSource>().clip = audioClip;
+    }
+
+    // OnStateUpdate is called before OnStateUpdate is called on any state inside this state machine
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        // Only play clip once per state
+        if (played) { return; }
+
+        animator.transform.root.gameObject.GetComponent<AudioSource>().PlayOneShot(audioClip);
+        played = true;
+    }
+
+    // OnStateExit is called before OnStateExit is called on any state inside this state machine
+    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    
+    //}
+
+    // OnStateMove is called before OnStateMove is called on any state inside this state machine
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    
+    //}
+
+    // OnStateIK is called before OnStateIK is called on any state inside this state machine
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    
+    //}
+
+    // OnStateMachineEnter is called when entering a state machine via its Entry Node
+    //override public void OnStateMachineEnter(Animator animator, int stateMachinePathHash)
+    //{
+    //}
+
+    // OnStateMachineExit is called when exiting a state machine via its Exit Node
+    //override public void OnStateMachineExit(Animator animator, int stateMachinePathHash)
+    //{
+    //    
+    //}
+}
